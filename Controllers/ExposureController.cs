546e027befa496d5196b93331350c129839470b6
Sukhdip Singh <sukhdip944@gmail.com>
2026-03-30 15:18:03 +0200

    chore: annotate ProducesResponseType on ExposureController endpoints

--- Controllers/ExposureController.cs
+++ Controllers/ExposureController.cs
@@ -13,6 +13,9 @@ public class ExposureController : ControllerBase
     public ExposureController(ExposureService exposure) => _exposure = exposure;
 
     [HttpPost("run")]
+    [ProducesResponseType(typeof(ExposureResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
     public async Task<IActionResult> RunExposure([FromBody] ExposureRequest req)
     {
         try
@@ -25,6 +28,8 @@ public class ExposureController : ControllerBase
     }
 
     [HttpGet("history")]
+    [ProducesResponseType(typeof(List<ExposureResult>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetHistory([FromQuery] string machineId)
     {
         if (string.IsNullOrEmpty(machineId))
