4adc3ae0141a93993629833e8b305e5ac6d702d1
Sukhdip Singh <sukhdip944@gmail.com>
2026-03-31 16:50:24 +0200

    chore: remove abandoned EventSourcing IEventStore interface

--- EventSourcing/IEventStore.cs
+++
@@ -1,9 +0,0 @@
-namespace LithoTwinAPI.EventSourcing;
-
-// Started looking into Event Sourcing for the FSM transitions 
-// but it's way too complicated for now. Sticking to EF Core.
-public interface IEventStore
-{
-    // Task AppendEventAsync(string streamId, object @event);
-    // Task<IEnumerable<object>> ReadEventsAsync(string streamId);
-}
